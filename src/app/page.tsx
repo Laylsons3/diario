2c958ffe827884e91642fca7088f602b44e2ab56
Laylson Sousa <laylsons3@gmail.com>
2023-07-20 13:36:28 -0300

    remodelando o layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,84 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
+interface DataJson {
+  motorista: string;
+  placa: string;
+  localOrigem: string;
+  kmInicial: number;
+  motivo: string;
+  destino: string;
+}
+
 export default function Home() {
+  const { register, handleSubmit } = useForm<DataJson>();
+
+  const submit = (data: DataJson) => {
+    console.log(data);
+    localStorage.setItem("diario", JSON.stringify(data));
+  };
+
   return (
     <div>
       <h1>Diário de bordo</h1>
-      <form className="flex p-4 bg-zinc-700 w-full max-w-sm flex-col gap-y-4">
+      <form
+        onClick={handleSubmit(submit)}
+        className="flex p-4 bg-zinc-700 w-full max-w-xs flex-col gap-y-2"
+      >
         <div>
-          <label className="flex w-full gap-x-1">
-            Motorista:
-            <input className="flex-1 rounded text-zinc-800 px-1" />
-          </label>
-        </div>
-        <div>
-          <label className="flex w-full gap-x-1">
-            Placa:
-            <input className="flex-1 uppercase rounded text-zinc-800 px-1" />
-          </label>
-        </div>
-        <div>
-          <label className="flex w-full gap-x-1">
-            Local de origem:
-            <input className="flex-1 rounded text-zinc-800 px-1" />
-          </label>
-        </div>
-        <div>
-          <label className="flex w-full gap-x-1">
-            Km inicial:
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Motorista:</span>
             <input
+              {...register("motorista")}
+              className="flex-1 rounded text-zinc-800 px-1"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Placa:</span>
+            <input
+              {...register("placa")}
+              className="flex-1 uppercase rounded text-zinc-800 px-1"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Local de origem:</span>
+            <input
+              {...register("localOrigem")}
+              className="flex-1 rounded text-zinc-800 px-1"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Km inicial:</span>
+            <input
+              {...register("kmInicial")}
               type="number"
               className="flex-1 rounded text-zinc-800 px-1"
             />
           </label>
         </div>
         <div>
-          <label className="flex w-full gap-x-1">
-            Motivo da viagem:
-            <input className="flex-1 rounded text-zinc-800 px-1" />
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Motivo da viagem:</span>
+            <input
+              {...register("motivo")}
+              className="flex-1 rounded text-zinc-800 px-1"
+            />
           </label>
         </div>
         <div>
-          <label className="flex w-full gap-x-1">
-            Destino:
-            <input className="flex-1 rounded text-zinc-800 px-1" />
+          <label className="flex flex-col w-full">
+            <span className="text-sm">Destino:</span>
+            <input
+              {...register("destino")}
+              className="flex-1 rounded text-zinc-800 px-1"
+            />
           </label>
         </div>
         <div className="text-center">
